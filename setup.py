1281e0a62d7974589225f25795cabb6ca2b6a5ef
Shiv Shankar S <shivshankarsajeev2000@gmail.com>
2025-05-26 22:41:01 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements(file_path: str) -> List[str]:
+    """
+    This fuction will return the list of requirements
+    """
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="Laptop-Price",
+    author="shiv shankar s",
+    author_email="shivshankarsajeev@gmail.com",
+    packages=find_packages(),
+    install_requiree=get_requirements("requirements.txt"),
+)
